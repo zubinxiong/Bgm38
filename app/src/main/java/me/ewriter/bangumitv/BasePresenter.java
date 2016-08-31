5ca3716e9196349908a68be3559de000d3820e91
zubinxiong <zubinxiong@gmail.com>
2016-08-31 11:51:40 +0800

    update build.config

---
+++ app/src/main/java/me/ewriter/bangumitv/BasePresenter.java
@@ -0,0 +1,11 @@
+package me.ewriter.bangumitv;
+
+/**
+ * Created by Zubin on 2016/8/31.
+ */
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+}
---
+++ app/src/main/java/me/ewriter/bangumitv/BaseView.java
@@ -0,0 +1,7 @@
+package me.ewriter.bangumitv;
+
+/**
+ * Created by Zubin on 2016/8/31.
+ */
+public interface BaseView<T> {
+}
