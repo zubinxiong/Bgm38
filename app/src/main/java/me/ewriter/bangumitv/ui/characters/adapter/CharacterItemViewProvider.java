945fbc83a924288ffb74fa43463ba0628c4c603a
zubinxiong <zubinxiong@gmail.com>
2016-09-30 15:53:03 +0800

    character can start PictureViewActivity

--- app/src/main/java/me/ewriter/bangumitv/ui/characters/adapter/CharacterItemViewProvider.java
+++ app/src/main/java/me/ewriter/bangumitv/ui/characters/adapter/CharacterItemViewProvider.java
@@ -1,6 +1,9 @@
 package me.ewriter.bangumitv.ui.characters.adapter;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import com.squareup.picasso.Picasso;
 
 import me.drakeet.multitype.ItemViewProvider;
 import me.ewriter.bangumitv.R;
+import me.ewriter.bangumitv.ui.picture.PictureViewActivity;
 
 /**
  * Created by Zubin on 2016/9/28.
@@ -27,7 +31,7 @@ public class CharacterItemViewProvider extends ItemViewProvider<CharacterItem, C
     }
 
     @Override
-    protected void onBindViewHolder(@NonNull ViewHolder holder, @NonNull CharacterItem characterItem) {
+    protected void onBindViewHolder(@NonNull final ViewHolder holder, @NonNull final CharacterItem characterItem) {
         Picasso.with(holder.avatar.getContext())
                 .load(characterItem.avatarUrl)
                 .fit()
@@ -40,6 +44,17 @@ public class CharacterItemViewProvider extends ItemViewProvider<CharacterItem, C
         holder.info.setText(characterItem.info);
         holder.cv.setText(characterItem.cvName);
 
+        holder.item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(holder.name.getContext(), PictureViewActivity.class);
+                intent.putExtra(PictureViewActivity.EXTRA_IMAGE_URL, characterItem.avatarUrl);
+                intent.putExtra(PictureViewActivity.EXTRA_IMAGE_TEXT, characterItem.name + "\n" + characterItem.cvName);
+                v.getContext().startActivity(intent,
+                        ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) v.getContext(), holder.avatar, "img").toBundle());
+            }
+        });
+
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
@@ -48,9 +63,11 @@ public class CharacterItemViewProvider extends ItemViewProvider<CharacterItem, C
         TextView name;
         TextView info;
         TextView cv;
+        View item;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            item = itemView;
             avatar = (ImageView) itemView.findViewById(R.id.character_image);
             name = (TextView) itemView.findViewById(R.id.character_name);
             info = (TextView) itemView.findViewById(R.id.character_info);
