412c678a40672c03c99f66114ea386950dba87c8
zubinxiong <zubinxiong@gmail.com>
2016-09-30 11:21:59 +0800

    add Immersive Mode on PictureViewActivity

--- app/src/main/java/me/ewriter/bangumitv/ui/picture/PictureViewActivity.java
+++ app/src/main/java/me/ewriter/bangumitv/ui/picture/PictureViewActivity.java
@@ -88,6 +88,19 @@ public class PictureViewActivity extends BaseActivity implements View.OnClickLis
         }
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
     private void setupToolbar() {
         mToolbar.setTitle("");
         mToolbar.setBackgroundColor(getResources().getColor(R.color.shadow_color));
